2e603bcaaccf1c3768a9420f0d5b29f7b3760a06
sibphl11 <asterales.niza@gmail.com>
2026-08-26 22:29:36 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -10,7 +10,35 @@
 
 const CONFIG = {
 
-    spreadsheetID: "1kYvLj9RiXRxZ5rpSvZw8AnNAspngHkY9CMFZKMX_Rno",
+    // DATA PENANAMAN PBPH
+    spreadsheetPenanaman:
+        "1kYvLj9RiXRxZ5rpSvZw8AnNAspngHkY9CMFZKMX_Rno",
+
+
+    // DATA KAWASAN HUTAN
+    spreadsheetKawasan:
+        "1mXXOn8m0jDvTPOnRmSY4rE61u5mUuX21HLyxmtI8lmE",
+
+
+    // DATA PRODUKSI KAYU BULAT
+    spreadsheetProduksi:
+        "11jcpMuO0i8Nh7cId1loAKYOHn6J9jRSNT3lzmT3t4VU",
+
+
+    // DATA PRODUKSI INDUSTRI
+    spreadsheetIndustri:
+        "1u9Wx1pUc2M-KzE9997_osg-8NHG5wH4etq4H9am7Rmc",
+
+};
+
+
+// =====================================================
+// URL API OPENSHEET
+// =====================================================
+
+const API_CONFIG = {
+
+    baseURL: "https://opensheet.elk.sh",
 
 };
 
@@ -29,7 +57,7 @@ const MAP_CONFIG = {
 
 
 // =====================================================
-// KONFIGURASI DATA
+// KONFIGURASI DATA LOKAL
 // =====================================================
 
 const DATA_CONFIG = {
